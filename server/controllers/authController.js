3c7c9ca1eba544a63e9ea1f3dc62df0c5c645c0c
jaronow <waronow123@gmail.com>
2020-04-26 14:48:46 -0700

    added resetPassword and getUsername to authController, will need to add actual email support using sendmail or something

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -38,5 +38,32 @@ module.exports = {
         msg: 'Server Error Occured, please try again'
       })
     }
+  },
+  // need to implement actual send email functionality for below routes
+  async resetPassword (req, res) {
+    try {
+      const user = await User.findOne({ email: req.body })
+      res.status(200).json({
+        msg: `Thank you, we have sent an email to ${req.body} with a link to reset your password.  This link will expire in 30 minutes`
+      })
+    } catch (e) {
+      console.log(e)
+      res.status(500).json({
+        msg: 'Server Error Occured, please try again'
+      })
+    }
+  },
+  async getUsername (req, res) {
+    try {
+      const user = await User.findOne({ email: req.body })
+      res.status(200).json({
+        msg: `Thank you, we have sent an email to ${req.body} with your user name if it exists`
+      })
+    } catch (e) {
+      console.log(e)
+      res.status(500).json({
+        msg: 'Server Error Occured, please try again'
+      })
+    }
   }
 }
