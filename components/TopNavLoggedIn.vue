89f670ad9be42bef7b2691a4214bb62c6c1b351c
jaronow <waronow123@gmail.com>
2020-04-27 13:48:23 -0700

    created dashboard page and link in nav bar

--- components/TopNavLoggedIn.vue
+++ components/TopNavLoggedIn.vue
@@ -1,6 +1,11 @@
 <template>
   <b-nav-item-dropdown :text="username" right>
-    <b-dropdown-item :to="/profile/ + id" class="text-center">Profile</b-dropdown-item>
+    <b-dropdown-item to="dashboard" class="text-center" v-if="role != user">
+      Dashboard
+    </b-dropdown-item>
+    <b-dropdown-item :to="/profile/ + id" class="text-center">
+      Profile
+    </b-dropdown-item>
     <hr>
     <b-button @click="logoutUser" size="sm" class="my-2 my-sm-2 mx-auto">
       Logout
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <section id="dashboard-container">
+    <h1>Dashboard Page</h1>
+    <h2>{{ user.username }}</h2>
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      user: state => state.user.user
+    })
+  },
+  head () {
+    return {
+      title: 'Employee Dashboard',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Main area to manage pets, contacts and things related to Pet Adopt site'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
