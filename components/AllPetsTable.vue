c862368a997b17a4630ba478db7ab8d2b2739a2f
jaronow <waronow123@gmail.com>
2020-04-29 13:56:37 -0700

    started to refractor pets vuex and routes to condense dogs/cats and any future pets into one schema using species

--- components/AllPetsTable.vue
+++ components/AllPetsTable.vue
@@ -5,7 +5,7 @@
         <b-button variant="primary" size="sm" :to="'dashboard/edit-pet/' + data.item._id">
           Edit
         </b-button>
-        <b-button variant="primary" size="sm" @click="markAdopted(data.item._id)">
+        <b-button variant="primary" size="sm" @click="markAdopted(data.item)">
           Mark Adopted
         </b-button>
         <b-button v-if="isAdmin" variant="primary" size="sm" @click="deletePet(data.item._id)">
@@ -41,7 +41,12 @@ export default {
       alert(id)
     },
     markAdopted (id) {
-      alert(id)
+      try {
+        alert(id)
+        console.log(id)
+      } catch (e) {
+        console.log(e.response.data.msg)
+      }
     }
   }
 }
--- store/pets.js
+++ store/pets.js
@@ -1,36 +1,29 @@
 export const state = () => ({
-  cats: [],
-  dogs: [],
-  currentPet: {}
+  pets: []
 })
 
 export const mutations = {
-  SET_CATS (state, cats) {
-    state.cats = cats
-  },
-  SET_DOGS (state, dogs) {
-    state.dogs = dogs
-  },
-  SET_CURRENT_PET (state, pet) {
-    state.currentPet = pet
+  SET_ALL_PETS (state, pets) {
+    state.pets = pets
   }
 }
 
 export const actions = {
-  async getPets ({ commit }) {
-    const dogs = await this.$axios.get('/dogs')
-    const cats = await this.$axios.get('/cats')
-    commit('SET_DOGS', dogs.data.dogs)
-    commit('SET_CATS', cats.data.cats)
+  async getAllPets ({ commit }) {
+    const pets = await this.$axios.get('/pets')
+    commit('SET_ALL_PETS', pets.data.pets)
   },
   async getSinglePet ({ commit }, { species, id }) {
-    const animal = species.substring(0, 4)
-    const pet = await this.$axios.get(`/${animal}/${id}`)
+    const route = species.substring(0, 4)
+    const pet = await this.$axios.get(`/${route}/${id}`)
     commit('SET_CURRENT_PET', pet.data.pet)
   },
   async addNewPet ({ dispatch }, { route, petForm }) {
     await this.$axios.post(`${route}`, petForm)
     dispatch('getPets')
+  },
+  async updatePet ({ dispatch }, { route, id, info }) {
+    await this.$axios.put(`${route}/${id}`, info)
   }
 }
 
