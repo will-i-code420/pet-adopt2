78d5d825064b58a84038832dea63811e7f326d56
jaronow <waronow123@gmail.com>
2020-04-29 13:07:18 -0700

    created all pet table component for displaying list of all pets on dashboard, added new route to edit-pet by id in dashboard

---
+++ components/AllPetsTable.vue
@@ -0,0 +1,44 @@
+<template>
+  <section id="all-pets-table">
+    <b-table hover :items="allPets" :fields="fields">
+      <template v-slot:cell(action)="data">
+        <b-button variant="primary" size="sm" :to="'dashboard/edit-pet/' + data.item._id">
+          Edit
+        </b-button>
+        <b-button variant="primary" size="sm" @click="deletePet(data.item._id)">
+          Delete
+        </b-button>
+        <b-button variant="primary" size="sm" @click="markAdopted(data.item._id)">
+          Pet Adopted
+        </b-button>
+      </template>
+    </b-table>
+  </section>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  name: 'AllPetsTable',
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    deletePet (id) {
+      alert(id)
+    },
+    markAdopted (id) {
+      alert(id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
