1518c944b73e3417256d648cdaef360c03362a20
Will Aronow <waronow123@gmail.com>
2020-05-15 12:33:00 -0700

    created route/page for specific pet id and linked single pet componenet to it

---
+++ pages/pets/_species/_id/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section id="single-pet-container">
+    <b-container>
+      <b-row>
+        <b-col>
+          <SinglePetCard :pet="pet" />
+        </b-col>
+      </b-row>
+    </b-container>
+  </section>
+</template>
+
+<script>
+import SinglePetCard from '~/components/SinglePetCard'
+
+export default {
+  components: {
+    SinglePetCard
+  },
+  computed: {
+    pet () {
+      const pet = this.$store.getters['pets/getSelectedPet'](this.$route.params.id)
+      return pet[0]
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
