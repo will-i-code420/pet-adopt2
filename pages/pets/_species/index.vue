b0f68e051c82bc38a34940c69c7331e40590eb18
Will Aronow <waronow123@gmail.com>
2020-05-15 12:11:01 -0700

    created new route for pets + species name for one route to all pets, updated nav to use new route/page

---
+++ pages/pets/_species/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1>New Single Route Animal Page</h1>
+    <h2>{{ $route.params.species }}</h2>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
