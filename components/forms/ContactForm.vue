e7dda8f7d99be2fa3198bdb43bd777b5c36266c8
Will Aronow <waronow123@gmail.com>
2020-05-15 16:53:39 -0700

    refactored contact us form w/ new global component, added title slot to form componenet

--- components/forms/ContactForm.vue
+++ components/forms/ContactForm.vue
@@ -1,72 +1,86 @@
 <template>
-  <b-form align-h="start" class="pb-3">
-    <h3 class="text-center" v-if="!animalInquiry">
-      Contact Us
-    </h3>
-    <h3 class="text-center" v-else>
-      You're Interested In {{ pet.name }}
-    </h3>
-    <b-form-group
-      id="input-group-name"
-      label="Your Name:"
-      label-for="name"
-      :state="nameVerif"
-      :invalid-feedback="invalidName"
-      :valid-feedback="validName"
-    >
-    <b-form-input
-      id="name"
-      v-model="contactForm.name"
-      required
-      :state="nameVerif"
-      placeholder="Enter name"
-    ></b-form-input>
-    </b-form-group>
-    <b-form-group
-      id="input-group-email"
-      label="Your Email:"
-      label-for="email"
-    >
-    <b-form-input
-      id="email"
-      v-model="contactForm.email"
-      type="email"
-      required
-      placeholder="example@email.com"
-    ></b-form-input>
-    </b-form-group>
-    <b-form-group
-      id="input-group-question"
-      label="Question/Details:"
-      label-for="info"
-    >
-    <b-form-textarea
-      id="info"
-      v-model="contactForm.info"
-      placeholder="Enter question..."
-      required
-      rows="3"
-      max-rows="6"
-    ></b-form-textarea>
-    </b-form-group>
-    <b-button variant="primary" class="mt-3 px-5 py-2" @click="submitQuestion">
-      Contact Us
-    </b-button>
-    <b-alert
-      :show="error"
-      variant="danger"
-    >
-      {{ errorMsg }}
-    </b-alert>
-    <b-alert
-      :show="submitted"
-      dismissible
-      fade
-      variant="success"
-    >
-      {{ submittedMsg }}
-    </b-alert>
-  </b-form>
+  <Form>
+    <template #title>
+      <h3 class="text-center" v-if="!animalInquiry">
+        Contact Us
+      </h3>
+      <h3 class="text-center" v-else>
+        You're Interested In {{ pet.name }}
+      </h3>
+    </template>
+    <template #form-group-1>
+      <b-form-group
+        id="input-group-name"
+        label="Your Name:"
+        label-for="name"
+        :state="nameVerif"
+        :invalid-feedback="invalidName"
+        :valid-feedback="validName"
+      >
+      <b-form-input
+        id="name"
+        v-model="contactForm.name"
+        required
+        :state="nameVerif"
+        placeholder="Enter name"
+      ></b-form-input>
+      </b-form-group>
+    </template>
+    <template #form-group-2>
+      <b-form-group
+        id="input-group-email"
+        label="Your Email:"
+        label-for="email"
+      >
+      <b-form-input
+        id="email"
+        v-model="contactForm.email"
+        type="email"
+        required
+        placeholder="example@email.com"
+      ></b-form-input>
+      </b-form-group>
+    </template>
+    <template #form-group-3>
+      <b-form-group
+        id="input-group-question"
+        label="Question/Details:"
+        label-for="info"
+      >
+      <b-form-textarea
+        id="info"
+        v-model="contactForm.info"
+        placeholder="Enter question..."
+        required
+        rows="3"
+        max-rows="6"
+      ></b-form-textarea>
+      </b-form-group>
+    </template>
+    <template #btn1>
+      <b-button variant="primary" class="mt-3 px-5 py-2" @click="submitQuestion">
+        Contact Us
+      </b-button>
+    </template>
+    <template #errorMsg>
+      <b-alert
+        :show="error"
+        variant="danger"
+      >
+        {{ errorMsg }}
+      </b-alert>
+    </template>
+    <template #successMsg>
+      <b-alert
+        :show="submitted"
+        dismissible
+        fade
+        variant="success"
+      >
+        {{ submittedMsg }}
+      </b-alert>
+    </template>
+  </Form>
 </template>
 
 <script>
