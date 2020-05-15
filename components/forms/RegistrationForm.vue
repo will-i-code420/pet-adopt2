a564a5212fa41a708692565fc84cfd8ac359d767
Will Aronow <waronow123@gmail.com>
2020-05-15 16:36:49 -0700

    refactored registration form to use new global form component

--- components/forms/RegistrationForm.vue
+++ components/forms/RegistrationForm.vue
@@ -1,84 +1,98 @@
 <template>
-  <b-form>
-    <b-form-group
-      id="input-group-name"
-      label="Your Name:"
-      label-for="name"
-      :state="nameVerif"
-      :invalid-feedback="invalidName"
-      :valid-feedback="validName"
-    >
-    <b-form-input
-      id="name"
-      v-model="registrationForm.name"
-      required
-      :state="nameVerif"
-      placeholder="Enter name"
-    ></b-form-input>
-    </b-form-group>
-    <b-form-group
-      id="input-group-email"
-      label="Your Email:"
-      label-for="email"
-    >
-    <b-form-input
-      id="email"
-      v-model="registrationForm.email"
-      type="email"
-      required
-      placeholder="example@email.com"
-    ></b-form-input>
-    </b-form-group>
-    <b-form-group
-      id="input-group-username"
-      label="Username:"
-      label-for="username"
-      :state="usernameVerif"
-      :invalid-feedback="invalidUserName"
-      :valid-feedback="validUserName"
-    >
-    <b-form-input
-      id="username"
-      v-model="registrationForm.username"
-      required
-      :state="usernameVerif"
-      placeholder="Enter Username"
-    ></b-form-input>
-    </b-form-group>
-    <b-form-group
-      id="input-group-password"
-      label="Password:"
-      label-for="password"
-    >
-    <b-form-input
-      id="password"
-      v-model="registrationForm.password"
-      type="password"
-      required
-    ></b-form-input>
-    </b-form-group>
-    <b-form-group
-      id="input-group-confirm-password"
-      label="Confirm Password:"
-      label-for="confirm-password"
-    >
-    <b-form-input
-      id="confirm-password"
-      v-model="password2"
-      type="password"
-      required
-    ></b-form-input>
-    </b-form-group>
-    <b-button variant="primary" class="mt-3 mx-auto px-5 py-2" @click="registerUser">
-      Register
-    </b-button>
-    <b-alert
-      :show="error"
-      variant="danger"
-    >
-      {{ errorMsg }}
-    </b-alert>
-  </b-form>
+  <Form>
+    <template #form-group-1>
+      <b-form-group
+        id="input-group-name"
+        label="Your Name:"
+        label-for="name"
+        :state="nameVerif"
+        :invalid-feedback="invalidName"
+        :valid-feedback="validName"
+      >
+      <b-form-input
+        id="name"
+        v-model="registrationForm.name"
+        required
+        :state="nameVerif"
+        placeholder="Enter name"
+      ></b-form-input>
+      </b-form-group>
+    </template>
+    <template #form-group-2>
+      <b-form-group
+        id="input-group-email"
+        label="Your Email:"
+        label-for="email"
+      >
+      <b-form-input
+        id="email"
+        v-model="registrationForm.email"
+        type="email"
+        required
+        placeholder="example@email.com"
+      ></b-form-input>
+      </b-form-group>
+    </template>
+    <template #form-group-3>
+      <b-form-group
+        id="input-group-username"
+        label="Username:"
+        label-for="username"
+        :state="usernameVerif"
+        :invalid-feedback="invalidUserName"
+        :valid-feedback="validUserName"
+      >
+      <b-form-input
+        id="username"
+        v-model="registrationForm.username"
+        required
+        :state="usernameVerif"
+        placeholder="Enter Username"
+      ></b-form-input>
+      </b-form-group>
+    </template>
+    <template #form-group-4>
+      <b-form-group
+        id="input-group-password"
+        label="Password:"
+        label-for="password"
+      >
+      <b-form-input
+        id="password"
+        v-model="registrationForm.password"
+        type="password"
+        required
+      ></b-form-input>
+      </b-form-group>
+    </template>
+    <template #form-group-5>
+      <b-form-group
+        id="input-group-confirm-password"
+        label="Confirm Password:"
+        label-for="confirm-password"
+      >
+      <b-form-input
+        id="confirm-password"
+        v-model="password2"
+        type="password"
+        required
+      ></b-form-input>
+      </b-form-group>
+    </template>
+    <template #btn1>
+      <b-button variant="primary" class="mt-3 mx-auto px-5 py-2" @click="registerUser">
+        Register
+      </b-button>
+    </template>
+    <template #errorMsg>
+      <b-alert
+        :show="error"
+        variant="danger"
+      >
+        {{ errorMsg }}
+      </b-alert>
+    </template>
+  </Form>
 </template>
 
 <script>
