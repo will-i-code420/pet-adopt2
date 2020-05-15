a33057f13e3a65e545271bdbf1982fb69e0d5f23
Will Aronow <waronow123@gmail.com>
2020-05-15 15:20:27 -0700

    created generic form template with named slots to replace all forms

---
+++ components/common/Form.vue
@@ -0,0 +1,35 @@
+<template>
+  <b-form>
+    <slot name="form-group-1">
+    </slot>
+    <slot name="form-group-2">
+    </slot>
+    <slot name="form-group-3">
+    </slot>
+    <slot name="form-group-4">
+    </slot>
+    <slot name="form-group-5">
+    </slot>
+    <slot name="form-group-6">
+    </slot>
+    <slot name="form-group-7">
+    </slot>
+    <slot name="form-group-8">
+    </slot>
+    <slot name="submitBtn">
+    </slot>
+    <slot name="errorMsg">
+    </slot>
+    <slot name="successMsg">
+    </slot>
+  </b-form>
+</template>
+
+<script>
+export default {
+  name: 'Form'
+}
+</script>
+
+<style scoped>
+</style>
