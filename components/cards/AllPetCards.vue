a9115afc02df8bd571ed5d989ac6cd4bc8637fc5
Will Aronow <waronow123@gmail.com>
2020-05-16 13:09:08 -0700

    added new cards files, fixed error with single pet card container

---
+++ components/cards/AllPetCards.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-card
+    :title="pet.name"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 25rem;"
+    class="mb-4 text-center"
+  >
+    <b-card-text>
+      <p>Breed: {{ pet.breed }}</p>
+      <p>Age: {{ pet.age }}</p>
+      <p>{{ pet.description.substring(0, 75) + '...' }}</p>
+    </b-card-text>
+    <nuxt-link :to="$route.params.species + '/' + pet._id">
+      View Details
+    </nuxt-link>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'AllPetCards',
+  props: {
+    pet: Object
+  }
+}
+</script>
+
+<style scoped>
+</style>
