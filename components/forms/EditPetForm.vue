5268bcf52a350800e783c27a13525fe9b71e17c5
Will Aronow <waronow123@gmail.com>
2020-05-16 13:03:48 -0700

    moved cards into folder, updated all pages using them to new location, removed duplicate section tags from children componenets

--- components/forms/EditPetForm.vue
+++ components/forms/EditPetForm.vue
@@ -1,176 +1,174 @@
 <template>
-  <section id="edit-pet-container" class="my-5 text-center">
-    <Form>
-      <template #title>
-        <h3>
-          Editing {{ pet.name }}
-        </h3>
-      </template>
-      <template #form-group-1>
-        <b-form-group label="Animal Species">
-          <b-form-radio-group
-            id="species"
-            v-model="editPetForm.species"
-            :options="animalOptions"
-            name="species-options"
-            required
-          ></b-form-radio-group>
-        </b-form-group>
-      </template>
-      <template #form-group-2>
-        <b-form-group
-          id="input-group-name"
-          label="Pet Name:"
-          label-for="name"
-        >
-        <b-form-input
-          id="name"
-          v-model="editPetForm.name"
-          type="text"
+  <Form>
+    <template #title>
+      <h3>
+        Editing {{ pet.name }}
+      </h3>
+    </template>
+    <template #form-group-1>
+      <b-form-group label="Animal Species">
+        <b-form-radio-group
+          id="species"
+          v-model="editPetForm.species"
+          :options="animalOptions"
+          name="species-options"
           required
-          placeholder="Animal Name"
-        ></b-form-input>
-        </b-form-group>
-      </template>
-      <template #form-group-3>
-        <b-form-group
-          id="input-group-breed"
-          label="Pet Breed:"
-          label-for="breed"
-        >
-        <b-form-input
-          id="breed"
-          v-model="editPetForm.breed"
-          type="text"
+        ></b-form-radio-group>
+      </b-form-group>
+    </template>
+    <template #form-group-2>
+      <b-form-group
+        id="input-group-name"
+        label="Pet Name:"
+        label-for="name"
+      >
+      <b-form-input
+        id="name"
+        v-model="editPetForm.name"
+        type="text"
+        required
+        placeholder="Animal Name"
+      ></b-form-input>
+      </b-form-group>
+    </template>
+    <template #form-group-3>
+      <b-form-group
+        id="input-group-breed"
+        label="Pet Breed:"
+        label-for="breed"
+      >
+      <b-form-input
+        id="breed"
+        v-model="editPetForm.breed"
+        type="text"
+        required
+        placeholder="Animal Breed"
+      ></b-form-input>
+      </b-form-group>
+    </template>
+    <template #form-group-4>
+      <b-form-group label="Animal Gender">
+        <b-form-radio-group
+          id="gender"
+          v-model="editPetForm.gender"
+          :options="genderOptions"
+          name="gender-options"
           required
-          placeholder="Animal Breed"
-        ></b-form-input>
-        </b-form-group>
-      </template>
-      <template #form-group-4>
-        <b-form-group label="Animal Gender">
-          <b-form-radio-group
-            id="gender"
-            v-model="editPetForm.gender"
-            :options="genderOptions"
-            name="gender-options"
-            required
-          ></b-form-radio-group>
-        </b-form-group>
-      </template>
-      <template #form-group-5>
-        <b-form-group
-          id="input-group-age"
-          label="Pet Age:"
-          label-for="age"
-        >
-        <b-form-input
-          id="age"
-          v-model.number="editPetForm.age"
-          type="number"
-          required
-          placeholder="Animal Age"
-        ></b-form-input>
-        </b-form-group>
-      </template>
-      <template #form-group-6>
-        <div class="special-notes-container">
-          <p>Special Notes:</p>
-          <b-button variant="primary" class="my-3 px-5 py-2" @click="addNewNote">
-            New Note
-          </b-button>
-          <b-row v-show="addingNewNote">
-            <b-col offset="2">
+        ></b-form-radio-group>
+      </b-form-group>
+    </template>
+    <template #form-group-5>
+      <b-form-group
+        id="input-group-age"
+        label="Pet Age:"
+        label-for="age"
+      >
+      <b-form-input
+        id="age"
+        v-model.number="editPetForm.age"
+        type="number"
+        required
+        placeholder="Animal Age"
+      ></b-form-input>
+      </b-form-group>
+    </template>
+    <template #form-group-6>
+      <div class="special-notes-container">
+        <p>Special Notes:</p>
+        <b-button variant="primary" class="my-3 px-5 py-2" @click="addNewNote">
+          New Note
+        </b-button>
+        <b-row v-show="addingNewNote">
+          <b-col offset="2">
+            <b-form inline>
+              <b-input-group prepend="New Note">
+                <b-form-input
+                  id="notes"
+                  class="mr-3"
+                  v-model="newNote"
+                  type="text"
+                  ></b-form-input>
+              </b-input-group>
+              <b-button variant="primary" class="px-2 py-2 mr-2" @click="saveNewNote">
+                Save
+              </b-button>
+              <b-button variant="danger" class="py-2" @click="cancelNewNote">
+                X
+              </b-button>
+            </b-form>
+          </b-col>
+        </b-row>
+        <ul class="special-notes">
+          <li v-show="!editingNote" class="my-3" v-for="(note, index) in editPetForm.notes" :key="index">
+            {{note}}
+            <b-button variant="primary" @click="editNote(index, note)">
+              Edit
+            </b-button>
+            <b-button variant="danger" @click="deleteNote(index)">
+              X
+            </b-button>
+          </li>
+          <b-row v-show="editingNote">
+            <b-col offset="3">
               <b-form inline>
-                <b-input-group prepend="New Note">
-                  <b-form-input
-                    id="notes"
-                    class="mr-3"
-                    v-model="newNote"
-                    type="text"
-                    ></b-form-input>
-                </b-input-group>
-                <b-button variant="primary" class="px-2 py-2 mr-2" @click="saveNewNote">
+                <b-form-input
+                  id="notes"
+                  class="mr-3"
+                  v-model="newNote"
+                  type="text"
+                ></b-form-input>
+                <b-button variant="primary" class="px-2 py-2 mr-2" @click="saveNoteEdit">
                   Save
                 </b-button>
-                <b-button variant="danger" class="py-2" @click="cancelNewNote">
+                <b-button variant="danger" class="py-2" @click="cancelNoteEdit">
                   X
                 </b-button>
               </b-form>
             </b-col>
           </b-row>
-          <ul class="special-notes">
-            <li v-show="!editingNote" class="my-3" v-for="(note, index) in editPetForm.notes" :key="index">
-              {{note}}
-              <b-button variant="primary" @click="editNote(index, note)">
-                Edit
-              </b-button>
-              <b-button variant="danger" @click="deleteNote(index)">
-                X
-              </b-button>
-            </li>
-            <b-row v-show="editingNote">
-              <b-col offset="3">
-                <b-form inline>
-                  <b-form-input
-                    id="notes"
-                    class="mr-3"
-                    v-model="newNote"
-                    type="text"
-                  ></b-form-input>
-                  <b-button variant="primary" class="px-2 py-2 mr-2" @click="saveNoteEdit">
-                    Save
-                  </b-button>
-                  <b-button variant="danger" class="py-2" @click="cancelNoteEdit">
-                    X
-                  </b-button>
-                </b-form>
-              </b-col>
-            </b-row>
-          </ul>
-        </div>
-      </template>
-      <template #form-group-7>
-        <b-form-group
-          id="input-group-description"
-          label="Pet Description:"
-          label-for="description"
-        >
-        <b-form-textarea
-          id="description"
-          v-model="editPetForm.description"
-          placeholder="Enter description..."
-          required
-          rows="3"
-          max-rows="6"
-        ></b-form-textarea>
-        </b-form-group>
-      </template>
-      <template #btn1>
-        <b-button variant="primary" class="my-3 px-5 py-2" @click="submitPetEdits">
-          Submit Changes
-        </b-button>
-      </template>
-      <template #errorMsg>
-        <b-alert
-          :show="error"
-          variant="danger"
-        >
-          {{ errorMsg }}
-        </b-alert>
-      </template>
-      <template #successMsg>
-        <b-alert
-          :show="submitted"
-          dismissible
-          fade
-          variant="success"
-        >
-          {{ submittedMsg }}
-        </b-alert>
-      </template>
-    </Form>
-  </section>
+        </ul>
+      </div>
+    </template>
+    <template #form-group-7>
+      <b-form-group
+        id="input-group-description"
+        label="Pet Description:"
+        label-for="description"
+      >
+      <b-form-textarea
+        id="description"
+        v-model="editPetForm.description"
+        placeholder="Enter description..."
+        required
+        rows="3"
+        max-rows="6"
+      ></b-form-textarea>
+      </b-form-group>
+    </template>
+    <template #btn1>
+      <b-button variant="primary" class="my-3 px-5 py-2" @click="submitPetEdits">
+        Submit Changes
+      </b-button>
+    </template>
+    <template #errorMsg>
+      <b-alert
+        :show="error"
+        variant="danger"
+      >
+        {{ errorMsg }}
+      </b-alert>
+    </template>
+    <template #successMsg>
+      <b-alert
+        :show="submitted"
+        dismissible
+        fade
+        variant="success"
+      >
+        {{ submittedMsg }}
+      </b-alert>
+    </template>
+  </Form>
 </template>
 
 <script>
@@ -263,7 +261,5 @@ export default {
 </script>
 
 <style scoped>
-#edit-pet-container {
-  border: 2px solid black;
-}
+
 </style>
