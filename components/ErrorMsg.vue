02c1b5c458504fdb2e827b8b3330c13997f5c0d2
Will Aronow <waronow123@gmail.com>
2020-05-18 21:22:21 -0700

    created error msg component

---
+++ components/ErrorMsg.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="success-msg-container">
+    <b-alert
+      :show="error"
+      variant="danger"
+      class="text-center"
+    >
+      {{ errorMsg }}
+    </b-alert>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'ErrorMsg',
+  computed: {
+    ...mapGetters({
+      error: 'messages/getErrorStatus',
+      errorMsg: 'messages/getErrorMsg'
+    })
+  }
+}
+</script>
+
+<style scoped>
+</style>
