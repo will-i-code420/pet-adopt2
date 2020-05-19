8bfd3ffd141651ae44f4d77d122ceda13976bdee
Will Aronow <waronow123@gmail.com>
2020-05-18 21:24:40 -0700

    made error msg component global to refactor its use throughout app

--- plugins/global-components.js
+++ plugins/global-components.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Form from '~/components/common/Form'
+import ErrorMsg from '~/components/common/ErrorMsg'
 
 const components = {
-  Form
+  Form,
+  ErrorMsg
 }
 
 Object.entries(components).forEach(([name, component]) => {
