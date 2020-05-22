3602bc5ad7e48cbe533d000458f98a9f3f0a0d0b
Will Aronow <waronow123@gmail.com>
2020-05-22 12:21:30 -0700

    added edit user route to routes, created update function in auth controller policy

--- server/policies/authControllerPolicy.js
+++ server/policies/authControllerPolicy.js
@@ -93,6 +93,49 @@ module.exports = {
       next()
     }
   },
+  update (req, res, next) {
+    const schema = Joi.object({
+      name: Joi.string().min(3).required(),
+      email: Joi.string().email({ minDomainSegments: 2 }).required(),
+      username: Joi.string().min(4).max(30).required(),
+      password: Joi.string().regex(/^[a-zA-Z0-9]{8,32}$/),
+    })
+    const { error, value } = schema.validate(req.body)
+    if (error) {
+      switch(error.details[0].context.key) {
+        case 'name':
+          res.status(409).json({
+            msg: 'Please provide valid name, at least 3 characters'
+          })
+        break;
+        case 'email':
+        res.status(409).json({
+          msg: 'Please provide valid email address'
+        })
+        break;
+        case 'username':
+        res.status(409).json({
+          msg: 'Please create a username between 4 and 30 characters in length'
+        })
+        break;
+        case 'password':
+        res.status(409).json({
+          msg: `Provided password failed to match 1 or both rules:
+          <br>
+          1. Must contain the following characters: lowercase, uppercase, or numbers. No spaces or special characters
+          <br>
+          2. Must be between 8 and 32 characters in length`
+        })
+        break;
+        default:
+        res.status(409).json({
+          msg: 'An Error Occured, please try again'
+        })
+      }
+    } else {
+      next()
+    }
+  },
   reset (req, res, next) {
     const schema = Joi.object({
       email: Joi.string().email({ minDomainSegments: 2 }).required()
