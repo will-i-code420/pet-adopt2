ccca3385fdf4020089dbd285a57ecd98bc17035e
Will Aronow <waronow123@gmail.com>
2020-05-22 15:12:04 -0700

    created change password form

---
+++ components/forms/ChangePasswordForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <section id="change-password-form">
+    <Form>
+      <template #form-group-1>
+        <b-form-group
+          id="input-group-currentPassword"
+          label="Current Password:"
+          label-for="currentPassword"
+        >
+        <b-form-input
+          id="currentPassword"
+          v-model="changePasswordForm.currentPassword"
+          type="password"
+          required
+        ></b-form-input>
+        </b-form-group>
+      </template>
+      <template #form-group-2>
+        <b-form-group
+          id="input-group-password"
+          label="New Password:"
+          label-for="password"
+        >
+        <b-form-input
+          id="password"
+          v-model="changePasswordForm.newPassword"
+          type="password"
+          required
+        ></b-form-input>
+        </b-form-group>
+      </template>
+      <template #form-group-3>
+        <b-form-group
+          id="input-group-confirmPassword"
+          label="Confirm Password:"
+          label-for="confirmPassword"
+        >
+        <b-form-input
+          id="confirmPassword"
+          v-model="cpassword"
+          type="password"
+          required
+        ></b-form-input>
+        </b-form-group>
+      </template>
+      <template #btn>
+        <b-button variant="primary" class="my-3 mr-3" @click="submitPasswordChange">
+          Submit
+        </b-button>
+      </template>
+    </Form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ChangePasswordForm'
+}
+</script>
+
+<style scoped>
+</style>
